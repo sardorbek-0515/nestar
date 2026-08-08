966fad8c7cbebff8619e89e28b40b76a9dfa7f9b
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-08-08 22:55:06 +0900

    feat: configure env veriables

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// define
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT_API ?? 3000);
 }
+
+// call
 bootstrap();
