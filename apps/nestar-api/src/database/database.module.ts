81b396388ac9ec05610870a34850cf9aabcf4a4f
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-08-12 01:11:49 +0900

    feat: create Schema Models and project enums

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -1,27 +1,39 @@
-import { Module } from '@nestjs/common';
-import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
-import { Connection } from 'mongoose';
+import { Module } from "@nestjs/common";
+import { InjectConnection, MongooseModule } from "@nestjs/mongoose";
+import { Connection } from "mongoose";
 
 @Module({
-	imports: [
-		MongooseModule.forRootAsync({
-			useFactory: () => ({
-				uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
-			}),
-		}),
-	],
-	exports: [MongooseModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => {
+        console.log("NODE_ENV:", process.env.NODE_ENV);
+        console.log("MONGO_DEV:", process.env.MONGO_DEV);
+
+        return {
+          uri:
+            process.env.NODE_ENV === "production"
+              ? process.env.MONGO_PROD
+              : process.env.MONGO_DEV,
+        };
+      },
+    }),
+  ],
+  exports: [MongooseModule],
 })
 export class DatabaseModule {
-	constructor(@InjectConnection() private readonly connection: Connection) {
-		if (connection.readyState === 1) {
-			console.log(
-				`MongoDB is connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} db`,
-			);
-		} else {
-			{
-				console.log('DB is not connected!');
-			}
-		}
-	}
+  constructor(
+    @InjectConnection() private readonly connection: Connection,
+  ) {
+    if (connection.readyState === 1) {
+      console.log(
+        `MongoDB is connected into ${
+          process.env.NODE_ENV === "production"
+            ? "production"
+            : "development"
+        } db`,
+      );
+    } else {
+      console.log("DB is not connected!");
+    }
+  }
 }
