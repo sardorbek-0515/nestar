d38687c595ae005c98289a435eb2d822c2ef2f66
sardor <sardor@sardors-MacBook-Pro.local>
2026-08-24 15:27:18 +0900

    feat: integration of Authentication and Authorization

--- apps/nestar-api/src/components/auth/auth.service.ts
+++ apps/nestar-api/src/components/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
 import { Member } from '../../libs/dto/member/member';
 import { T } from '../../libs/types/common';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 
 @Injectable()
@@ -25,13 +26,13 @@ export class AuthService {
 			payload[`${ele}`] = member[`${ele}`];
 		});
 		delete payload.MemberPassword;
-		
 		return await this.jwtService.signAsync(payload);
 	}
 
 	/**====================== verifyToken ======================= **/
 	public async verifyToken(token: string): Promise<Member> {
 		const member = await this.jwtService.verifyAsync(token);
+		member._id = shapeIntoMongoObjectId(member._id);
 		return member;
 	}
 }
--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,8 +1,11 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException } from '@nestjs/common';
+import { InternalServerErrorException, UseGuards } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
 
 @Resolver()
 export class MemberResolver {
@@ -21,19 +24,33 @@ export class MemberResolver {
 		console.log('Mutation: login');
 		return this.memberService.login(input);
 	}
+
+
     // Authenticated bolgan userlar kra oladi updateMemberga (user.admin.agent)
 	/** --------------------------- updateMember --------------------------- **/
+	@UseGuards(AuthGuard)
 	@Mutation(() => String) // @Mutation(POST)
-	public async updateMember(): Promise<string> {
+	public async updateMember(@AuthMember ('_id')  memberId: ObjectId): Promise<string> {
 		console.log('Mutation: updateMember');
+		// console.log(typeof memberId);
+		// console.log(memberId);
 		return this.memberService.updateMember();
 	}
-
+ 
 	/** --------------------------- getMember --------------------------- **/
 	@Query(() => String) // @Query (GET)
 	public async getMember(): Promise<string> {
 		console.log('Query: getMember');
 		return this.memberService.getMember();
+	}
+
+	@UseGuards(AuthGuard)
+	@Query(() => String) // @Mutation(POST)
+	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
+		console.log('Query: checkAuth');
+		console.log('memberNick:', memberNick);
+		console.log(memberNick);
+		return `Hi ${memberNick}`;
 	}
 
 	/** --------------------------- getAllMembers --------------------------- **/ 
@@ -43,7 +60,7 @@ export class MemberResolver {
 		console.log('Mutation: getAllMembers');
 		return this.memberService.getAllMembers();
 	}
-
+    
 	// Authhorization: ADMIN faqat
 	@Mutation(() => String)
 	public async updateMemberByAdmin(): Promise<string> {
