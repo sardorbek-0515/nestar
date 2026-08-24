f597233149c46191975cb900d0d7eb2627313612
sardor <sardor@sardors-MacBook-Pro.local>
2026-08-24 17:39:28 +0900

    feat: develop getMember GrapQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -10,6 +10,7 @@ import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class MemberResolver {
@@ -63,10 +64,11 @@ export class MemberResolver {
 	}
 
 	/** --------------------------- getMember --------------------------- **/
-	@Query(() => String) // @Query (GET)
-	public async getMember(): Promise<string> {
+	@Query(() => Member) // @Query (GET)
+	public async getMember(@Args("memberId") input: string): Promise<Member> {
 		console.log('Query: getMember');
-		return this.memberService.getMember();
+		const targetId = shapeIntoMongoObjectId(input);
+		return this.memberService.getMember(targetId);
 	}
 
 	/** --------------------------- getAllMembersByAdmin --------------------------- **/
