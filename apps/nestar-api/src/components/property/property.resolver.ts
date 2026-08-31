647dd03bd459fc03a2ff59aee19db04e79a3d883
sardor <sardor@sardors-MacBook-Pro.local>
2026-08-31 18:50:44 +0900

    feat: develop updatePropertyByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -97,6 +97,16 @@ export class PropertyResolver {
 		return await this.propertyService.getAllPropertiesByAdmin(input);
 	}
 
+	/**==== updatePropertyByAdmin ===== **/
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async updatePropertyByAdmin(@Args('input') input: PropertyUpdate): Promise<Property> {
+		console.log('Mutation: updatePropertyByAdmin');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.propertyService.updatePropertyByAdmin(input);
+	}
+
 
 	
 }
