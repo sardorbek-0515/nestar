853599d34d84abadbefe27cbec6e44cb9f15d831
sardor <sardor@sardors-MacBook-Pro.local>
2026-09-04 16:53:01 +0900

    feat: create likeService model

---
+++ apps/nestar-api/src/components/like/like.service.ts
@@ -0,0 +1,14 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Like, MeLiked } from '../../libs/dto/like/like';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { T } from '../../libs/types/common';
+import { Message } from '../../libs/enums/common.enum';
+
+@Injectable()
+export class LikeService {
+	constructor(@InjectModel('Like') private readonly likeModel: Model<Like>) {}
+
+	
+}
