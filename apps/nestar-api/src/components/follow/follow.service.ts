193600a8cfc73447863273197a1b2e59fce18d22
sardor <sardor@sardors-MacBook-Pro.local>
2026-09-07 02:01:17 +0900

    feat: integration of lookupAuthMemberFollowed complex query business logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -6,7 +6,7 @@ import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
-import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberFollowed, lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 
 
 
@@ -84,7 +84,10 @@ export class FollowService {
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
 							lookupAuthMemberLiked(memberId, "$followingId"),
-							// meFollowed
+						    lookupAuthMemberFollowed({
+								 followerId: memberId, 
+								 followingId: "$followingId" 
+								}),
 							lookupFollowingData,
 							{ $unwind: '$followingData' },
 						],
@@ -116,7 +119,10 @@ export class FollowService {
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
 							lookupAuthMemberLiked(memberId, "$followerId"),//followeridga like bosganmi
-							// meFollowed
+							lookupAuthMemberFollowed({
+								 followerId: memberId, 
+								 followingId: "$followerId" 
+								}),//followeridga follow bosganmi
 							lookupFollowerData,
 							{ $unwind: '$followerData' },
 						],
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -19,6 +19,7 @@ export const availableCommentSorts = ['createdAt', 'updatedAt'];
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
 import { T } from './types/common';
+import { Interface } from 'readline';
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/webp', 'application/octet-stream'];
 export const getSerialForImage = (filename: string) => {
@@ -60,6 +61,46 @@ export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = '$_id')
 				},
 			],
 			as: 'meLiked',
+		},
+	};
+};
+
+
+interface LookupAuthMemberFollowed {
+	followerId: T,
+	followingId: string;
+}
+export const lookupAuthMemberFollowed = (input: LookupAuthMemberFollowed) => {
+	const { followerId, followingId } = input;
+	return {
+		$lookup: {
+			from: 'follows',
+			let: {
+				localFollowerId: followerId,
+				localFollowingId: followingId,
+				localMyFavorite: true,
+			},
+			pipeline: [
+				{
+					$match: {
+						$expr: {
+							$and: [
+								{ $eq: ['$followerId', '$$localFollowerId'] },
+								{ $eq: ['$followingId', '$$localFollowingId'] },
+							],
+						},
+					},
+				},
+				{
+					$project: {
+						_id: 0,
+						followerId: 1,
+						followingId: 1,
+						myFollowing: '$$localMyFavorite',
+					},
+				},
+			],
+			as: 'meFollowed',
 		},
 	};
 };
